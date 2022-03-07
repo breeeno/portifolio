117c839a84a5b27847fde228e00fe15bc3b24b86
Breno <bx.sptf@gmail.com>
2022-03-07 02:13:34 -0300

    - Instalado depedências e feito fix no settings

--- webdev/settings.py
+++ webdev/settings.py
@@ -132,7 +132,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-    COLLECTFAST_ENABLED = True
+    COLLECTFAST_ENABLED = False
 
     AWS_DEFAULT_ACL = 'private'
 
