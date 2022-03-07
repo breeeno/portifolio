4eab0e40bd8b1f3f3b5da541bb7fa3598e771f5a
Breno <bx.sptf@gmail.com>
2022-03-07 01:54:18 -0300

    - Fix na palavra errada :p

--- webdev/settings.py
+++ webdev/settings.py
@@ -117,7 +117,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-AWS_ACCESS_KEY_ID = config('AWS_ACESS_KEY_ID', default=False)
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=False)
 
 # STORAGE CONFIGURATION IN S3 AWS
 # -------------------------------------------------------
