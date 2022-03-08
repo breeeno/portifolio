13399c183bae8c663db347089ee719e7a702900e
Breno <bx.sptf@gmail.com>
2022-03-07 22:18:12 -0300

    - Implementado base do template

--- webdev/settings.py
+++ webdev/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'webdev.principal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
