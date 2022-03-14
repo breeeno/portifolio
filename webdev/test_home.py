7a7154a1ce6d9490e4cf46ed357f5b4d1ce13888
Victor Augusto <victor.soares@irricontrol.com.br>
2022-03-13 23:56:08 -0300

    some tests to home page

--- webdev/test_home.py
+++ webdev/test_home.py
@@ -1,6 +1,21 @@
-from django.test import Client
+import pytest
+from django.urls import reverse
+
+from django_assertions import assert_contains
 
 
-def test_home_status_code(client: Client):
-    resposta = client.get('')
-    assert resposta.status_code == 200
+@pytest.fixture
+def resp(client, db):
+    return client.get(reverse('principal:home'))
+
+
+def test_home_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Breno Eustáquio - Portifólio</title>')
+
+
+def test_home_title_link(resp):
+    assert_contains(resp, 'href="#page-top">Breno Eustáquio</a>')
